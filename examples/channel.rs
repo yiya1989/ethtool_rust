873013e7d5cf7be9e8edfd23c09f70ef3c9bb333
liuchen.gele <liuchen.gele@bytedance.com>
2022-11-02 16:14:23 +0800

    add README.md

--- examples/channel.rs
+++ examples/channel.rs
@@ -4,31 +4,27 @@ fn main() {
     let channel_info_result = ethtool::get_channel_info("eth0");
 
     let channel_info = match channel_info_result {
-        Ok(channel_info) => {
-            println!("channel_info1: {}", channel_info);
-            channel_info
-        }
+        Ok(channel_info) => channel_info,
         Err(e) => {
             panic!("get channel_info failed, {}", e)
         }
     };
-    println!("current channel_info {}", channel_info);
-    println!("max_combined1 {}", channel_info.max_combined);
+    println!("current channel_info: {}", channel_info);
+    println!("max_combined1: {}", channel_info.max_combined);
 
-    let mut new_set_combined =
+    let mut new_combined_value =
         std::cmp::min(channel_info.max_combined, channel_info.combined_count) - 1;
 
-    new_set_combined = std::cmp::max(1, new_set_combined);
+    new_combined_value = std::cmp::max(1, new_combined_value);
 
-    let channel_info2 = ethtool::set_channel_combined("eth0", new_set_combined);
+    let channel_info2 = ethtool::set_channel_combined("eth0", new_combined_value);
     let max_combined2 = match channel_info2 {
         Ok(channel_info) => {
             println!("channel_info2: {}", channel_info);
             channel_info.combined_count
         }
         Err(e) => {
-            println!("max_combined error {}", e);
-            0
+            panic!("set_channel_combined failed, {}", e)
         }
     };
     println!("max_combined2 {}", max_combined2);
@@ -40,8 +36,7 @@ fn main() {
             channel_info.combined_count
         }
         Err(e) => {
-            println!("max_combined error {}", e);
-            0
+            panic!("set_channel_combined failed, {}", e)
         }
     };
     println!("max_combined3 {}", max_combined3);
